ea9483acae2d0dab0fa29ceefe484fdcbc6fec07
Federico Spadaro <federicospadaro2@gmail.com>
2025-05-29 14:56:45 +0200

    milestone 2

--- Amazon-App/src/App.jsx
+++ Amazon-App/src/App.jsx
@@ -12,9 +12,10 @@ function debounce(callback, delay) {
   }
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [query, setQuery] = useState ('');
-  const [suggestions, setSuggestions] = useState([]);
+
+const [products, setProducts] = useState ([]);
+const [query, setQuery] = useState('');
+const [suggestions, setSuggestions] = useState([]);
 
 useEffect(() => {
   fetch(`http://localhost:3333/products`)
@@ -22,72 +23,90 @@ useEffect(() => {
   .then(data => {
     setProducts(data);
   })
-  .catch(err => console.error(err));
-}, []);
+  .catch(err => console.error("Errore nel recupero dei dati dal fetch:", err));
+}, [])
+
+useEffect(() => {
+
+if (query === '') {
+      setSuggestions([]);
+      return;
+    }
+
+  fetch(`http://localhost:3333/products?search=${query}`)
+  .then(res => res.json())
+  .then(data => {
+    setSuggestions(data);
+  })
+  .catch(err => console.error("Errore nel recupero dei dati per suggerimeto", err));
+}, [query])
 
 const handleSearch = useCallback(
     debounce((value) => {
       setQuery(value.toLowerCase().trim());
     }, 200), []);
 
-useEffect(() => {
-  if (query.leght === '') {
-    setSuggestions([]);
-    return;
-  }
-
-  fetch(`http://localhost:3333/products?search=${query}`)
-    .then(res => res.json())
-    .then(data => {
-      setSuggestions(data);
-    })
-    .catch(err => {
-      console.error('Errore nella ricerca suggerimenti:', err);
-      setSuggestions([]);
-    });
-}, [query]);
-const filteredProducts = useMemo(() => {
-  return products.filter((product => {
-    return product.name.toLowerCase().includes(query) ||
-    product.description.toLowerCase().includes(query)
-  }))
+const filteredProduct = useMemo(() => {
+  return products.filter(product => 
+    product.name.toLowerCase().includes(query.toLowerCase().trim()) || 
+    product.description.toLowerCase().includes(query.toLowerCase().trim())
+  );
 }, [products, query]);
 
-return (
-  <div>
-    <h1>Lista prodotti amazon</h1>
-    <input 
-    type="text" 
-    value={query}
-    onChange={(e) => handleSearch(e.target.value)}
-    />
+return(
+  <div className="GenericContainer">
+    <div className="searchContainer">
+      <input type="text" placeholder="Cerca un prodotto..." value={query} onChange={(e) => handleSearch(e.target.value)}/>
+      
+      {suggestions.length > 0 && (
+        <ul className="suggestionsList">
+          {suggestions.map((suggestion) => (
+            <li key={suggestion.id} onClick={() => {
+              setQuery(suggestion.name.toLowerCase().trim());
+              setSuggestions([]);
+            }}>
+              {suggestion.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
 
-{query && suggestions.length > 0 && (
-  <ul className="suggestions-dropdown">
-    {suggestions.map((item) => (
-      <li
-        key={item.id}
-        onMouseDown={() => {
-          setQuery(item.name);
-          setSuggestions([]);
-        }}
-      >
-        {item.name}
-      </li>
-    ))}
-  </ul>
-)}
+    <div className="productsContainer" >
+    {filteredProduct.map((product) => {
+      return (
+        <div className="productCard" key={product.id}>
+          <img
+            src={`https://via.placeholder.com/500x500?text=${encodeURIComponent(product.name)}`}
+            alt={product.name}
+          />
+          <h2>{product.name}</h2>
+          <p>{product.description}</p>
+          <span>{product.price} €</span>
+        </div>
+      )
+    })}
+    </div>
 
+    <div className="suggestionsContainer">
 
-    {filteredProducts.map((product) => (
-      <div key={product.id}>
-        <h2>{product.name}</h2>
-        <p>{product.description}</p>
-        <p>Prezzo: {product.price}€</p>
+      
+
+      <div className="suggestionsMessage">
+        {filteredProduct.map((product) => (
+          <div className="productCard" key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <span>{product.price} €</span>
+          </div>
+        ))}
       </div>
-    ))}
+
+    </div>
+
   </div>
 )
-  
+
 }
+
 export default App;
